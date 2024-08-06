804e28ce3856ea25c2d1826de6b18fb96a9e734f
Rushikesh Chandavale <rushichandavale@gmail.com>
2024-08-06 23:04:07 +0530

    reverse string

--- dropxout/05reverse.js
+++ dropxout/05reverse.js
@@ -1,4 +1,4 @@
-
+console.log("using loop")
 function reverseStr(str){
     var revStr = '';
     for(let i = str.length-1; i>=0;i--){
@@ -9,3 +9,12 @@ function reverseStr(str){
 
 let result = reverseStr("hello")
 console.log(result)
+
+console.log('using pre-defined functions')
+
+function revStr(str){
+    return str.split('').reverse().join('')
+}
+
+let res = revStr("hello world")
+console.log( res)
