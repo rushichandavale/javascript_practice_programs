29655f0666189f88ce65d5a42dee3e37d3af2761
Rushikesh Chandavale <rushichandavale@gmail.com>
2024-08-06 21:17:34 +0530

    loops added

---
+++ dropxout/02loop.js
@@ -0,0 +1,24 @@
+console.log('using for loop')
+
+
+for(let i = 1; i<=5;i++){
+    console.log('Number is :',i)
+}
+
+
+console.log("using while loop")
+
+let i = 1;
+while(i<=5){
+    console.log('Number is :',i);
+    i++;
+}
+
+console.log('using do while')
+
+let j=1;
+do {
+    
+    console.log('Number is :',j);
+    j++
+} while (j<=5);
